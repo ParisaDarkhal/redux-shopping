265537f7535eaa7d4629ab6184c2d9ae5c7f225b
ParisaDarkhal <p.darkhal@gmail.com>
2023-07-31 20:36:14 -0400

    products created

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Button, Card } from "react-bootstrap";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -11,7 +12,24 @@ const Products = () => {
   return (
     <>
       <p>Products dashboard</p>
-      {JSON.stringify(products)}
+      <div className="row">
+        {products.map((product) => (
+          <div className="col-md-3" key={product.id}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                src={product.image}
+                style={{ width: "100px", height: "130px" }}
+              />
+              <Card.Body>
+                <Card.Title>{product.title}</Card.Title>
+                <Card.Text> UDS {product.price}</Card.Text>
+                <Button variant="primary">Add To Cart</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
